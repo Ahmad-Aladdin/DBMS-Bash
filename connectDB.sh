095ef057b9bfb2cc6f7623da9ee762b56b8ab4d1
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2021-12-31 20:45:38 +0200

    SPecial char handling

--- connectDB.sh
+++ connectDB.sh
@@ -6,15 +6,19 @@ if  [[   $db_name = ""   ]]
 then
 echo "NOthing is entered,PLease enter again" 
 read db_name
-#end of NOthing is entered
 
-elif [ -d $db_name ] 
+elif [ -d $db_name ] # if the dir exist ? connect 
 then
 	cd $db_name
 	echo "successfully connected to $db_name"
 
+elif ! [[ $db_name == *[a-z][A-Z][0-9] ]];then
+	 #cannot check on dot and /
+	#special char check
+	#how to match regx in bash [[ ]] 
+	echo " you have entered special characters"
 else
-	echo " No DB Named $db_name "
-	read db_name
+	echo " No DB Named $db_name " #dir does not exist
+
 fi
 
--- dropDB.sh
+++ dropDB.sh
@@ -5,11 +5,17 @@ if [ -d $db_name ]
 then
 	rm -r $db_name 2> /dev/null #put any error in the cmd
 	echo " $db_name sucessfully dropped"
+
+elif ! [[ $db_name == *[a-zA-Z0-9] ]] ; then
+	 #cannot check on dot and /
+	#special char check
+	#how to match regx in bash [[ ]] 
+	echo " you have entered special characters"
 else
 	if [ $db_name = "" ]
 		then
 		echo "NOthing is entered,PLease enter again" 
-		read db_name
+		
 	else 
 	    	echo " this db does not exist"
 	fi
