ee8e6f763ea942c2927b45ad59cf3ac535ce1e3f
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2021-12-31 22:06:40 +0200

    TablesMenu

---
+++ TablesMenu.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+exitMenu=0
+while true
+do 
+select choice in "Create Table" "List Tables" "Drop Tables" "Insert into  Table" "Select into Table" "Delete from Table" "Update Table" "exit Menu"
+do 
+case $choice in
+"Create Table") echo "Create Table"
+		. ./CreateTabe.sh
+		break
+		;;
+"List Tables") echo "List Tables"
+		. ./ListTables.sh
+		break
+		;;
+"Drop Tables") echo "Drop Tables"
+		. ./DropTable.sh
+		break
+		;;
+"Insert into  Table") echo "Insert into  Table"
+		. ./insertRecord.sh
+		break
+		;;
+"Select into Table") echo "Select into Table"
+		. ./select.sh
+		break
+		;;
+"Delete from Table") echo "Delete from Table"
+		. ./deleteRecord.sh
+		break
+		;;
+"Update Table") echo "Update Table"
+		. ./update.sh
+		break
+		;;
+"exitMenu") exitMenu=1 
+		 break
+		 ;;
+esac
+done
+if [ $exitMenu == 1 ]; 
+then
+	break
+fi
+done
