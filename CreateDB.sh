f0cb4409d81523f360ba4f9a8a83a84f3131864b
Ahmed-Aladdin <a_aladdin@A.Aladdin>
2022-01-01 02:18:18 +0200

    bug fixes

--- CreateDB.sh
+++ CreateDB.sh
@@ -11,12 +11,18 @@ echo -e "Enter Database Name: \c"
             echo -e "\e[41mYou can't enter these characters => . / : - | \e[0m"
             echo press any key
             read
-    elif [[ -e $dbName ]]; 
+    elif [[ -d ./Databases/$dbName ]]; 
     then
 		echo -e "\e[41mthis database name is already used\e[0m"
 		echo press any key
 		read
     else
+        DIR="./Databases"
+        if [ ! -d "$DIR" ]; then
+        # Take action if $DIR exists. #
+         mkdir ./Databases 
+        fi
+       # mkdir ./Databases
         mkdir ./Databases/$dbName  
         if [[ $? == 0 ]]
         then
@@ -25,3 +31,6 @@ echo -e "Enter Database Name: \c"
         echo "Error Creating Database $dbName"
         fi
     fi
+
+
+    
